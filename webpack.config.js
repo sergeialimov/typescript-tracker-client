7f6027d7fcd44385aae82944d1aa558b88143e21
sergeialimov <8912652@gmail.com>
2020-10-16 12:58:22 +0300

    add config for typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,65 @@
+const path = require('path');
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
       },
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }
-        ]
-      }
-    ]
-  }
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
+
+// module.exports = {
+//   entry: [
+//     './src/index.tsx'
+//   ],
+//   output: {
+//     path: __dirname,
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader"
+//         }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           {
+//             loader: "style-loader"
+//           },
+//           {
+//             loader: "css-loader",
+//             options: {
+//               modules: true,
+//               importLoaders: 1,
+//               localIdentName: "[name]_[local]_[hash:base64]",
+//               sourceMap: true,
+//               minimize: true
+//             }
+//           }
+//         ]
+//       }
+//     ]
+//   }
+// };
